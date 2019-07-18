793198589ae57531719a9b62a4eef633aa04899a
Alex Chui <ahhchui@gmail.com>
2019-07-17 18:07:21 -0700

    cached interactive dom elements

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const turnStatement_p = document.getElementById("turn-statement");
+
+const topLeft_div = document.getElementById("top-left")
+const topMiddle_div = document.getElementById("top-middle")
+const topRight_div = document.getElementById("top-right")
+const midLeft_div = document.getElementById("mid-left")
+const midMiddle_div = document.getElementById("mid-middle")
+const midRight_div = document.getElementById("mid-right")
+const botLeft_div = document.getElementById("bot-left")
+const botMiddle_div = document.getElementById("bot-middle")
+const botRight_div = document.getElementById("bot-right")
+
+const result_div = document.querySelector(".result")
+
+
+
