1856f7a23b4103eca04baefc183e61f0f8afe834
Alex Chui <ahhchui@gmail.com>
2019-07-26 19:55:28 -0700

    added game stop functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const compMark = "O";
 let playerIndices = [];
 let compIndices = [];
 
+const resultBox_div = document.querySelector('.game-result')
 const winningCombos = [
   [0,1,2],
   [3,4,5],
@@ -15,25 +16,45 @@ const winningCombos = [
   [2,4,6]
 ];
 
-//var board = ["", "", "", "", "", "", "", "", ""];
+var board = ["", "", "", "", "", "", "", "", ""];
 const boxes = document.querySelectorAll('.game-box');
 
 
 function reset() {
   for (let i = 0; i < boxes.length; i++) {
-    //    board[i] = "";
+    board[i] = "";
     boxes[i].innerHTML = "";
     boxes[i].style.removeProperty("background-color", "#ACA")
     boxes[i].addEventListener("click", playerClick);
   };
 };
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, sleep));
+};
+
 function playerClick(box) {
   markBox(box.target.id, playerMark);
+  let won = winner(playerMark);  
+  if (won) {
+    displayWinner(playerMark);
+    return;
+  };
+  compIndex = compChooseBox();
+  sleep(1000).then(compClick(compIndex))
+  won = winner(compMark);
+  if (won) {
+    displayWinner(compMark);
+    return;
+  };
 };
 
 function compChooseBox() {
-
+  let emptyIndices = []
+  for (let i = 0; i < board.length; i ++) {
+    if ( board[i] == "" ) { emptyIndices.push(i) }
+  };
+  return emptyIndices[Math.floor(Math.random() * emptyIndices.length)];
 };
 
 function compClick(boxId) {
@@ -41,7 +62,7 @@ function compClick(boxId) {
 };
 
 function markBox(boxId, mark) {
-  //  board[boxId] = mark;
+  board[boxId] = mark;
   document.getElementById(boxId).innerHTML = mark;
   if ( mark == "X" ) {playerIndices.push(parseInt(boxId))}
   else if ( mark == "O" ) {compIndices.push(parseInt(boxId))};
@@ -58,7 +79,7 @@ function winner(mark) {
     };
   } else if (mark == "O") {
     for (let j = 0; j < winningCombos.length; j++) {
-      if ( winningCombos[i].every(idx => compIndices.includes(idx)) ) return true;
+      if ( winningCombos[j].every(idx => compIndices.includes(idx)) ) return true;
     };
   };
   return false
@@ -73,10 +94,20 @@ function tie() {
 function gameOver() {
   if (winner("X") || winner("O") || tie()) {return true};
   return false;
-}
+};
+
+function displayWinner(mark) {
+  for (i = 0; i < board.length; i++) {
+    boxes[i].removeEventListener('click', playerClick);    
+  };
+  resultBox_div.innerHTML = "${mark} WINS!";
+  resultBox_div.style.setProperty("display", "block");
+};
 
 function game() {
   reset();
+
 };
+
 
 game();
